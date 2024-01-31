3510e1aa996335ca3b6b9103a756e9bc55df4498
98013 <40274669+98013@users.noreply.github.com>
2024-01-31 22:57:26 +0530

    iterator patter,proxy Pattern, Observer Pattern

--- designpatterns.js
+++ designpatterns.js
@@ -81,42 +81,39 @@ console.log(Object.is(c1, c2, c3));
  */
 const ShippingDetailsInfo = {
     name: 'naseer',
-    ShippingDetails: function (state) {
-        switch (state) {
-            case 'ups': {
-                return {
-                    name: this.name,
-                    country: 'india',
-                };
-            }
-            case 'upsa': {
-                return {
-                    name: this.name,
-                    country: 'USA',
-                };
-            }
-            case 'fedex': {
-                return {
-                    name: this.name,
-                    country: 'USA',
-                };
-            }
-            default: {
-                return {
-                    name: '',
-                    country: '',
-                };
-            }
-        }
+    ShippingDetails1: function () {
+        return {
+            name: this.name,
+            country: 'India',
+        };
+    },
+    ShippingDetails2: function () {
+        return {
+            name: this.name,
+            country: 'USA',
+        };
+    },
+    ShippingDetails3: function () {
+        return {
+            name: this.name,
+            country: 'UK',
+        };
+    },
+    default: function () {
+        return {
+            name: '',
+            country: '',
+        };
     },
 };
 
 const stratergyPattern = Object.create(ShippingDetailsInfo);
-console.log(stratergyPattern);
-console.log(stratergyPattern.ShippingDetails('ups'));
-console.log(stratergyPattern.ShippingDetails('upsa'));
-console.log(stratergyPattern.ShippingDetails('fedex'));
-console.log(stratergyPattern.ShippingDetails(''));
+console.log(stratergyPattern.name);
+console.log(stratergyPattern.__proto__);
+console.log(stratergyPattern.ShippingDetails1('ups'));
+console.log(stratergyPattern.ShippingDetails2('upsa'));
+console.log(stratergyPattern.ShippingDetails3('fedex'));
+console.log(stratergyPattern.default(''));
 
 /**
  * Stratergy Pattern with Reflect.
@@ -136,3 +133,86 @@ function executeStrategy(strategyName) {
 
 console.log(executeStrategy('strategy1')); // "Result of strategy1"
 console.log(executeStrategy('strategy2')); // "Result of strategy2"
+
+/**
+ * Iterator Pattern - Behavioral Design Pattern.
+ * it allows us to define our own set of rules, to traverse over some collection of Objects.
+ */
+
+const items = [1, 'devsage', false, 1.24];
+function Iterator(items) {
+    this.items = items;
+    this.index = 0;
+}
+
+Iterator.prototype = {
+    hasNext: function () {
+        return this.index >= this.items.length
+            ? 'Index cannot be greater than Array Length.'
+            : `You can fetch the next item from the Array ${this.index}.`;
+    },
+    next: function () {
+        return this.items[this.index++];
+    },
+};
+
+const iterator = new Iterator(items);
+console.log(iterator);
+console.log(iterator.hasNext());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.hasNext());
+console.log(iterator.next());
+console.log(iterator.hasNext());
+
+/** Observer Pattern - Behavioral Design Pattern.
+ * where we define one to many dependency relationship, from one object know as subject to many other objects know as the Observers.
+ * Observers are functions, which watch the Subject wait for signal or trigger from the subject before they run.
+ */
+
+function Subject() {
+    this.observers = []; // Array of Observer functions.
+}
+
+Subject.prototype = {
+    subscribe: function (subsciberFn) {
+        [].concat(this.observers.push(subsciberFn));
+    },
+    unsubscribe: function (unsubscribeFn) {
+        this.observers.splice(this.observers.indexOf(unsubscribeFn), 1);
+    },
+    firedrill: function () {
+        return this.observers.map((fn, index) => fn());
+    },
+};
+
+const Observer1 = function () {
+    return 'function 1 gets called.';
+};
+
+const Observer2 = function () {
+    return 'function 2 gets called.';
+};
+
+const Observer3 = function () {
+    return 'function 3 gets called';
+};
+
+const subject = new Subject();
+subject.subscribe(Observer1);
+subject.subscribe(Observer2);
+subject.subscribe(Observer3);
+
+subject.unsubscribe(Observer1);
+
+console.log(subject.firedrill());
+
+/**
+ * Proxy Pattern Structural Design Pattern.
+ *  It provides a surrogate or placeholder for another object, allowing you to control access to it.
+ *  This pattern is particularly useful in situations where you need to add an extra layer of control, lazy loading, or remote access to Objects
+ */
+
+
+
