9398ae46c544af858c47fb40f70f7126348119f6
98013 <40274669+98013@users.noreply.github.com>
2024-02-02 19:50:38 +0530

    proxy design pattern

--- designpatterns.js
+++ designpatterns.js
@@ -214,5 +214,44 @@ console.log(subject.firedrill());
  *  This pattern is particularly useful in situations where you need to add an extra layer of control, lazy loading, or remote access to Objects
  */
 
+function CryptoCurrencyAPI() {
+    this.givenValue = function (coin) {
+        console.log('External API.');
+        switch (coin) {
+            case 'Bitcoin': {
+                return 'BitCoin 1';
+            }
+            case 'Litecoin': {
+                return 'LiteCoin 2';
+            }
+            case 'Etherium': {
+                return 'Etherium 3';
+            }
+        }
+    };
+}
 
+function ProxyObjectDailer() {
+    this.ObjectDailer = Object.create(new CryptoCurrencyAPI());
+    this.cache = {};
+    this.pushKeys = function (coin) {
+        console.log(coin);
+        console.log(this.cache);
+        if (this.cache[coin] == null) {
+            this.cache[coin] = this.ObjectDailer.givenValue(coin);
+        }
+        return this.cache;
+    };
+}
 
+const objectDailer = new ProxyObjectDailer();
+console.log(objectDailer.pushKeys('Bitcoin'));
+console.log(objectDailer.pushKeys('Etherium'));
+console.log(objectDailer.pushKeys('Litecoin'));
+console.log(objectDailer.pushKeys('Bitcoin'));
+console.log(objectDailer.pushKeys('Bitcoin'));
+console.log(objectDailer.pushKeys('Bitcoin'));
+console.log(objectDailer.pushKeys('Bitcoin'));
+console.log(objectDailer.pushKeys('Bitcoin'));
+console.log(objectDailer.pushKeys('Bitcoin'));
+console.log(objectDailer.pushKeys('Litecoin'));
